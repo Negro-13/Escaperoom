c665d363569ece545825cfc703b2a593aa88a72c
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2025-04-07 11:58:43 -0300

    Pistas

--- escaperoom/Nodos/Pistas/pistas_(area_2d).gd
+++ escaperoom/Nodos/Pistas/pistas_(area_2d).gd
@@ -6,14 +6,12 @@ func _ready():
 	$Label.visible = false
 
 func _on_body_entered(body):
-	if body.name == "Personaje": # cambia este nombre si tu personaje tiene otro nombre
-		jugador_cerca = true
-		$Label.visible = true
+	jugador_cerca = true
+	$Label.visible = true
 
 func _on_body_exited(body):
-	if body.name == "Personaje":
-		jugador_cerca = false
-		$Label.visible = false
+	jugador_cerca = false
+	$Label.visible = false
 
 func _process(_delta):
 	if jugador_cerca and Input.is_action_just_pressed("interactuar"): # debes crear esta acción en InputMap
